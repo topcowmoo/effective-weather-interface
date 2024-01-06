a37f26a760874a7f6d7a6fa18887c1820443c69f
Salvatore <mammoliti.sam@gmail.com>
2024-01-06 14:03:05 -0500

    created search button, history button, city input, current weather and forcast card variables to link them to index

---
+++ script.js
@@ -0,0 +1,5 @@
+const searchButton = document.querySelector (".search-btn")
+const historyButtonContainer = document.querySelector (".history-btns")
+const cityInput = document.querySelector (".city-input")
+const currentWeatherDiv = document.querySelector (".current-weather")
+const forcastCardDiv = document.querySelector (".forcast-cards")
