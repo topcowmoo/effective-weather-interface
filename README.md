487e9fdd24ab819357d16e38803922f13e080f30
Salvatore <mammoliti.sam@gmail.com>
2024-01-09 14:45:52 -0500

    "Addded API key variable to javascript. Changed city-input placeholder to Search... Added clear history button and added five-day forecast ul id and class"

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # effective-weather-interface
+
+pair programming wiht Sahil Banati 
+comment javascript lines and htn=ml and css
+screenshots
+deployed link
+github link
+take out console.logs
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const searchButton = document.querySelector (".search-btn")
-const historyButtonContainer = document.querySelector (".history-btns")
-const cityInput = document.querySelector (".city-input")
-const currentWeatherDiv = document.querySelector (".current-weather")
-const forcastCardDiv = document.querySelector (".forcast-cards")
+const searchButton = document.querySelector(".search-btn");
+const historyButtonContainer = document.querySelector(".history-btns");
+const cityInput = document.querySelector(".city-input");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const forcastCardDiv = document.querySelector(".forcast-cards");
+
+const apiKey = "329f03f3dd53cd0515673856c8dba1af";
