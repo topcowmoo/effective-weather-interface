155b45829c11004f73260161ef37233714412d03
Salvatore <mammoliti.sam@gmail.com>
2024-01-09 15:00:07 -0500

    Created cityCoordinates function which includes an api call to fetch lat, lon and city name

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ const currentWeatherDiv = document.querySelector(".current-weather");
 const forcastCardDiv = document.querySelector(".forcast-cards");
 
 const apiKey = "329f03f3dd53cd0515673856c8dba1af";
+
+function cityCoordinates() {
+    const cityName = cityInput.value.trim();
+    if (!cityName) return;
+
+    const geoCodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&appid=${apiKey}&limit=1`;
+
+    fetch(geoCodingApiUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const { name, lon, lat } = data[0];
+            getCurrentWeatherData(name, lon, lat);
+            getFiveDayForecast(name, lon, lat);
+            addToHistory(name);
+        })
+        .catch((error) => {
+            console.error("failed to fetch coordinates", error);
+        })
+        .finally(() => {
+                cityInput.value = ''; // Only clear input if it was not set by clicking history
+        });
+}
+
+function search() {
+    cityCoordinates();
+}
